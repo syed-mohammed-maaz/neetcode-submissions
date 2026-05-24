eb598e581307dc2194e78a143c229eb27526f02a
syed mohammed maaz <mohammedmaaz9158@gmail.com>
2026-05-24 22:45:41 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer,Integer> m=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            m.put(nums[i],m.getOrDefault(nums[i],0)+1);
+        }
+
+        for(int i:nums){
+            if(m.get(i)==1) return i;
+        }
+        return -1;
+    }
+}
