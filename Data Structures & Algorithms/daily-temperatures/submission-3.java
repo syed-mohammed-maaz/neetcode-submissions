bd9417acc0a4b47e003fc0887ac30b3706f88e19
syed mohammed maaz <mohammedmaaz9158@gmail.com>
2026-06-27 23:51:54 +0530

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.java
@@ -0,0 +1,66 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+    
+        int n=temperatures.length;
+        int[] result=new int[n];
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<n;i++){
+            int val=temperatures[i];
+            if(st.isEmpty()||temperatures[st.peek()]>=val){
+                st.push(i);
+            }else{
+                while(!st.isEmpty()&&temperatures[st.peek()]<val){
+                    int index=st.pop();
+                    result[index]=i-index;
+
+                }
+                st.push(i);
+            }
+        }
+        return result;
+    }
+}// class Solution {
+//     public int[] dailyTemperatures(int[] temperatures) {
+    
+//         int n=temperatures.length;
+//         int[] result=new int[n];
+
+//         Deque<Integer> st=new ArrayDeque<>();
+   
+//         for(int i=0;i<n;i++){
+//             int curEle=temperatures[i];
+//             if(st.isEmpty()||temperatures[st.peek()]>=curEle){
+//                 st.push(i);
+//             }else{
+//                 while(!(st.isEmpty())&&temperatures[st.peek()]<curEle){
+//                     int index=st.pop();
+//                     result[index]=i-index;
+//                 }
+//                 st.push(i);
+//             }
+//         }
+//         return result;
+//     }
+// }
+
+// class Solution {
+//     public int[] dailyTemperatures(int[] temperatures) {
+    
+//         int n=temperatures.length;
+//         int[] result=new int[n];
+//         int m=0;
+//         for(int i=0;i<n-1;i++){
+//             for(int j=i+1;j<n;j++){
+//                 if(temperatures[j]>temperatures[i]){
+//                     result[m++]=j-i;
+//                     break;            
+//                 }else if(j==n-1){
+//                     result[m++]=0;
+                 
+//                 }
+//             }
+//         }
+//         // result[m++]=0;
+//         return result;
+//     }
+// }
