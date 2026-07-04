0ab376513dd5a0a3aa36107335abbe45dd1c10b4
syed mohammed maaz <mohammedmaaz9158@gmail.com>
2026-07-05 00:05:21 +0530

    Add: reverse-a-linked-list - submission-2

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-2.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         Deque<Integer> st=new ArrayDeque<>();
+//         ListNode temp=head;
+//         while(temp!=null){
+//             st.push(temp.val);
+//             temp=temp.next;
+//         }
+
+//         temp=head;
+//         while(temp!=null){
+//             temp.val=st.pop();
+//             temp=temp.next;
+//         }
+//           return head;
+//     }
+  
+// }
+//using 3 poinnters
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//        ListNode prev=null;
+//        ListNode cur=head;
+//        ListNode next=null;
+//        if(cur!=null) next=cur.next;
+//        while(cur!=null){
+//         cur.next=prev;
+//         prev=cur;
+//         cur=next;
+//         if(cur!=null)next=cur.next;
+       
+//        }
+//        return prev;
+//     }
+  
+// }
+
+//more effiecent
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+}
